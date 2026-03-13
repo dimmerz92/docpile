53fe703e9df77f89c562f5a3a398a8fa06e9b4e4
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2026-03-13 20:47:15 +0800

    implement logger and pass tests

--- internal/core/logger.go
+++ internal/core/logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log/slog"
 	"maps"
+	"os"
 )
 
 var supportedLoggerFormats = map[string]struct{}{
@@ -18,6 +19,11 @@ var supportedLoggerLevels = map[string]slog.Level{
 	"error": slog.LevelError,
 }
 
+const (
+	LoggerDefaultFormat = "json"
+	LoggerDefaultLevel  = "warn"
+)
+
 var (
 	ErrLoggerFormat = fmt.Errorf("logger format not supported, use any of %v", maps.Keys(supportedLoggerFormats))
 	ErrLoggerLevel  = fmt.Errorf("logger level not supported, use any of %v", maps.Keys(supportedLoggerLevels))
@@ -28,6 +34,36 @@ type LoggerConfig struct {
 	Level  string `toml:"level"`
 }
 
-func (lc *LoggerConfig) Validate() error
+func (lc *LoggerConfig) Validate() error {
+	lc.Format = Coalesce(lc.Format, LoggerDefaultFormat)
+	if _, ok := supportedLoggerFormats[lc.Format]; !ok {
+		return ErrLoggerFormat
+	}
 
-func (lc *LoggerConfig) Init() error
+	lc.Level = Coalesce(lc.Level, LoggerDefaultLevel)
+	if _, ok := supportedLoggerLevels[lc.Level]; !ok {
+		return ErrLoggerLevel
+	}
+
+	return nil
+}
+
+func (lc *LoggerConfig) Init() error {
+	if err := lc.Validate(); err != nil {
+		return err
+	}
+
+	opts := &slog.HandlerOptions{Level: supportedLoggerLevels[lc.Level]}
+
+	var handler slog.Handler
+	switch lc.Format {
+	case "text":
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	default:
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	}
+
+	slog.SetDefault(slog.New(handler))
+
+	return nil
+}
