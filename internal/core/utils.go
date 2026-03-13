e7e3b59e0d4daac09d601a28806d7649b1a0d683
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2026-03-13 19:31:47 +0800

    add inline utils function signatures

---
+++ internal/core/utils.go
@@ -0,0 +1,8 @@
+package core
+
+// Coalesce returns the first non-zero value in values for type T.
+// If no non-zero value is found, the zero value for type T is returned.
+func Coalesce[T comparable](values ...T) T
+
+// IIF or inline-if that returns v1 if the condition is true, otherwise false.
+func IIF[T any](condition bool, v1, v2 T) T
